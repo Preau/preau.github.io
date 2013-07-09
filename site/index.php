afbad336923a66b49f82be8b164276a207099ffd
Preau <bitbucket@koenvanzuylen.com>
2013-07-09 23:08:19 +0200

    Update: Added new info to text

--- site/index.php
+++ site/index.php
@@ -8,7 +8,7 @@
 ?>
     <div class="headertext">Welkom</div>
     <p>
-        Mijn naam is Koen van Zuylen. Ik studeer Software Engineering aan de Avans Hogeschool in ‘s-Hertogenbosch. Op dit moment ben ik aan het afstuderen bij <a href="http://interactivestudios.nl/">Interactive Studios</a>. Op deze site houd ik mijn portfolio bij. Hierin staan onder andere projecten die ik voor school heb gemaakt en projecten die ik in mijn vrije tijd heb gemaakt.
+        Mijn naam is Koen van Zuylen. Ik ben programmeur bij <a href="http://interactivestudios.nl/">Interactive Studios</a>. Ik heb Informatica gestudeerd aan de Avans Hogeschool te 's-Hertogenbosch. Op deze site houd ik mijn portfolio bij. Hierin staan onder andere projecten die ik voor school heb gemaakt en projecten die ik in mijn vrije tijd heb gemaakt.
     </p>
 <?php
     //Load footer
