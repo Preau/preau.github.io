2b3ece32948afdef2722ff2e1d817679ff34f47c
Koen van Zuijlen <bitbucket@koenvanzuylen.com>
2014-12-19 23:59:28 +0100

    Removed portfolio and moved everything else to one page

--- builders/header.php
+++ builders/header.php
@@ -10,37 +10,12 @@
         <link rel="shortcut icon" type="image/x-icon" href="/style/favicon.ico" />
 		
         <script type="text/javascript" src="/plugins/google-analytics.js"></script>
-		
-        <script type="text/javascript" src="/plugins/jquery-1.7.2.min.js"></script>
-        <script type="text/javascript" src="/plugins/jqueryui/jquery-ui-1.8.21.custom.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="/plugins/jqueryui/jquery-ui-1.8.21.custom.css" />
-    
-        <script type="text/javascript" src="/plugins/shadowbox/shadowbox.js"></script>
-        <link rel="stylesheet" type="text/css" href="/plugins/shadowbox/shadowbox.css" />
         
         <link rel="stylesheet" type="text/css" href="/style/style.css" />
-        <script type="text/javascript" src="/script/script.js"></script>
     </head>
     <body>
         <div id="pagewrapper">
-			<div id="thepixel">
-			</div>
             <div id="logo">
                 Koen van Zuijlen
-            </div>
-            <div id="menu">
-                <?php
-                    //Create menuitems
-                    foreach($menuitems as $menuitemLink => $menuitemTitle) {
-                        $class = "menuitem";
-                        if($currentMenuitem == $menuitemLink) {
-                            $class .= "selected menuitem";
-                        }
-                        
-                        $menuitemHtml = '<div class="'.$class.'"><a href="/'.$menuitemLink.'/">'.$menuitemTitle.'</a></div>';
-                        echo $menuitemHtml;
-                    }
-                    echo '<br style="clear:both" />'
-                ?>
             </div>
             <div id="content">
--- index.php
+++ index.php
@@ -1,15 +1,81 @@
 <?php
-    //Setup Page
-    require_once("configuration/config.php");
-    $currentMenuitem = "index";
+    $errors = array();
+    $success = false;
+    if(!empty($_POST['submit'])) {
+        if(!empty($_POST['ditiseenpotmethoning'])) {
+            header('Location: /');
+            die();
+        }
+        //Check for errors
+        if(empty($_POST['name'])) {
+            $errors[] = "Vul een geldige naam in.";
+        }
+        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) || empty($_POST['email'])) {
+            $errors[] = "Vul een geldig e-mailadres in.";
+        }
+        if(empty($_POST['message'])) {
+            $errors[] = "Vul een geldig bericht in.";
+        }
+        if(!count($errors)) {
+            //Send mail
+            $to = "mail@koenvanzuijlen.com";
+            $replyto = $_POST['email'];
+            $headers = "From: contactform@koenvanzuijlen.com" . "\r\n" . "Reply-To: $replyto";
+            $subject = "Formulier Website";
+            $body = "Naam: ".$_POST['name']."\n\n".$_POST['message'];
+            if(mail($to, "Contactformulier Website", $body, $headers)) {
+                $success = true;
+            } else {
+                $errors[] = "Er is iets misgegaan bij het verzenden van het bericht.";
+            }
+        }
+    }
 
     //Load header
     require_once("builders/header.php");
 ?>
-    <div class="headertext">Welkom</div>
+    <h1>Welkom</h1>
     <p>
-        Mijn naam is Koen van Zuijlen. Ik ben programmeur bij <a href="http://interactivestudios.nl/">Interactive Studios</a>. Ik heb Informatica gestudeerd aan de Avans Hogeschool te 's-Hertogenbosch. Op deze site houd ik mijn portfolio bij. Hierin staan onder andere projecten die ik voor school heb gemaakt en projecten die ik in mijn vrije tijd heb gemaakt.
+        Mijn naam is Koen van Zuijlen. Ik ben programmeur bij <a href="http://interactivestudios.nl/">Interactive Studios</a>. Ik heb Informatica gestudeerd aan de Avans Hogeschool te 's-Hertogenbosch.
     </p>
+
+    <h1>Contact</h1>
+    <form method="post" action="">
+        <div id="contact">
+            <?php
+            if($success) {
+                echo '<p>';
+                echo "Bedankt voor je bericht!";
+                echo '</p>';
+            }
+            if(count($errors)) {
+                echo '<p style="color:#CC0000">';
+                foreach($errors as $error) {
+                    echo $error . '<br />';
+                }
+                echo '</p>';
+            }
+            ?>
+            <p>
+                <input id="name" class="contactinput" name="name" type="text"
+                    <?php if(count($errors)) echo 'value="'.$_POST['name'].'"'; ?>
+                    />
+                &nbsp;Naam
+                <br />
+                <input id="email" class="contactinput" name="email" type="text"
+                    <?php if(count($errors)) echo 'value="'.$_POST['email'].'"'; ?>
+                    />
+                &nbsp;E-Mailadres
+                <br />
+                <textarea id="message" class="contactinput" name="message" rows="10" cols="29"><?php if(count($errors)) echo $_POST['message']; ?></textarea>
+                <span style="vertical-align:top">&nbsp;Bericht</span>
+                <input id="ditiseenpotmethoning" class="contactinput" name="ditiseenpotmethoning" type="text" />
+            </p>
+            <p>
+                <input id="contactsubmitbutton" type="submit" name="submit" value="Verzenden"/>
+            </p>
+        </div>
+    </form>
 <?php
     //Load footer
     require_once("builders/footer.php");
