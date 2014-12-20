c8f02b196b063ff995adafa3bf23a1d924d173de
Koen van Zuijlen <bitbucket@koenvanzuylen.com>
2014-12-20 21:20:42 +0100

    New site using Bootstrap template

--- builders/footer.php
+++
@@ -1,7 +0,0 @@
-            </div>
-            <div id="footer">
-                <i>&copy; <?php echo date("Y") ?> - Koen van Zuijlen</i>
-            </div>
-        </div>
-    </body>
-</html>
--- index.php
+++ index.php
@@ -1,82 +1,248 @@
-<?php
-    $errors = array();
-    $success = false;
-    if(!empty($_POST['submit'])) {
-        if(!empty($_POST['ditiseenpotmethoning'])) {
-            header('Location: /');
-            die();
-        }
-        //Check for errors
-        if(empty($_POST['name'])) {
-            $errors[] = "Vul een geldige naam in.";
-        }
-        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) || empty($_POST['email'])) {
-            $errors[] = "Vul een geldig e-mailadres in.";
-        }
-        if(empty($_POST['message'])) {
-            $errors[] = "Vul een geldig bericht in.";
-        }
-        if(!count($errors)) {
-            //Send mail
-            $to = "mail@koenvanzuijlen.com";
-            $replyto = $_POST['email'];
-            $headers = "From: contactform@koenvanzuijlen.com" . "\r\n" . "Reply-To: $replyto";
-            $subject = "Formulier Website";
-            $body = "Naam: ".$_POST['name']."\n\n".$_POST['message'];
-            if(mail($to, "Contactformulier Website", $body, $headers)) {
-                $success = true;
-            } else {
-                $errors[] = "Er is iets misgegaan bij het verzenden van het bericht.";
-            }
-        }
-    }
+<!DOCTYPE html>
+<html lang="en">
 
-    //Load header
-    require_once("builders/header.php");
-?>
-    <h1>Welkom</h1>
-    <p>
-        Mijn naam is Koen van Zuijlen. Ik ben programmeur bij <a href="http://interactivestudios.nl/">Interactive Studios</a>. Ik heb Informatica gestudeerd aan de Avans Hogeschool te 's-Hertogenbosch.
-    </p>
+    <head>
 
-    <h1>Contact</h1>
-    <form method="post" action="">
-        <div id="contact">
-            <?php
-            if($success) {
-                echo '<p>';
-                echo "Bedankt voor je bericht!";
-                echo '</p>';
-            }
-            if(count($errors)) {
-                echo '<p style="color:#CC0000">';
-                foreach($errors as $error) {
-                    echo $error . '<br />';
-                }
-                echo '</p>';
-            }
-            ?>
-            <p>
-                <input id="name" class="contactinput" name="name" type="text"
-                    <?php if(count($errors)) echo 'value="'.$_POST['name'].'"'; ?>
-                    />
-                &nbsp;Naam
-                <br />
-                <input id="email" class="contactinput" name="email" type="text"
-                    <?php if(count($errors)) echo 'value="'.$_POST['email'].'"'; ?>
-                    />
-                &nbsp;E-Mailadres
-                <br />
-                <textarea id="message" class="contactinput" name="message" rows="10" cols="29"><?php if(count($errors)) echo $_POST['message']; ?></textarea>
-                <span style="vertical-align:top">&nbsp;Bericht</span>
-                <input id="ditiseenpotmethoning" class="contactinput" name="ditiseenpotmethoning" type="text" />
-            </p>
-            <p>
-                <input id="contactsubmitbutton" type="submit" name="submit" value="Verzenden"/>
-            </p>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Koen van Zuijlen</title>
+
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+        <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192">
+        <link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <meta name="msapplication-TileColor" content="#2b5797">
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+
+        <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/freelancer.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body id="page-top" class="index">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#page-top">Koen van Zuijlen</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#about">About</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#aroundtheweb">Around the Web</a>
+                        </li>
+                        <li class="page-scroll">
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+
+        <!-- Header -->
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <img class="img-responsive" src="img/profile.png" alt="">
+                        <div class="intro-text">
+                            <span class="name">Koen van Zuijlen</span>
+                            <hr class="star-light">
+                            <span class="skills">Web Developer at Interactive Studios</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <!-- About Section -->
+        <section id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>About</h2>
+                        <hr class="star-primary">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-lg-offset-2">
+                        <p>I'm a <?=(date("md", date("U", mktime(0, 0, 0, 2,15, 1991))) > date("md") ? ((date("Y") - 1991) - 1) : (date("Y") - 1991))?>-year-old web developer from Rosmalen, the Netherlands. I've studied software engineering at Avans Hogeschool in 's-Hertogenbosch. I'm currently employed at <a href="http://www.interactivestudios.nl/" target="_blank">Interactive Studios</a> where I create online software, mobile apps and websites.</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <p>I enjoy learning about and keeping up-to-date with all sorts of technology related stuff. Besides that my interests include listening to (mostly electronic) music, visiting music festivals and parties, photography, watching television series and movies and playing video games.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Find me Section -->
+        <section class="success" id="aroundtheweb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>Around the web</h2>
+                        <hr class="star-light">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <p>Find me online</p>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="https://www.facebook.com/koenvzuijlen" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/koenvanzuijlen" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/Preau" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-github"></i></a>
+                            </li>
+                            <li>
+                                <a href="https://bitbucket.org/Preau" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-bitbucket"></i></a>
+                            </li>
+                            <li>
+                                <a href="http://www.last.fm/user/DePreau" target="_blank" class="btn-social btn-outline"><i class="fa fa-fw fa-lastfm"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Contact Section -->
+        <section id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>Contact Me</h2>
+                        <hr class="star-primary">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
+                        <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
+                        <form name="sentMessage" id="contactForm" novalidate>
+                            <div class="row control-group">
+                                <div class="form-group col-xs-12 floating-label-form-group controls">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="row control-group">
+                                <div class="form-group col-xs-12 floating-label-form-group controls">
+                                    <label>Email Address</label>
+                                    <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="row control-group">
+                                <div class="form-group col-xs-12 floating-label-form-group controls">
+                                    <label>Message</label>
+                                    <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <br>
+                            <div id="success"></div>
+                            <div class="row">
+                                <div class="form-group col-xs-12">
+                                    <button type="submit" class="btn btn-success btn-lg">Send</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="text-center">
+            <div class="footer-below">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            Copyright &copy; Koen van Zuijlen <?=date('Y')?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+        <div class="scroll-top page-scroll visible-xs visble-sm">
+            <a class="btn btn-primary" href="#page-top">
+                <i class="fa fa-chevron-up"></i>
+            </a>
         </div>
-    </form>
-<?php
-    //Load footer
-    require_once("builders/footer.php");
-?>
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+        <script src="js/classie.js"></script>
+        <script src="js/cbpAnimatedHeader.js"></script>
+
+        <!-- Contact Form JavaScript -->
+        <script src="js/jqBootstrapValidation.js"></script>
+        <script src="js/contact_me.js"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="js/freelancer.js"></script>
+
+    </body>
+
+</html>
